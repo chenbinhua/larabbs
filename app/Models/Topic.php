810e7ab0d25ef5916e0514adbbf4adb1943a4a56
chenbinghua HS255 <HS255@huishengziben.com>
2018-06-11 15:51:02 +0800

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,16 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    //话题分类，一对一模型关联
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    //话题作者，一对一模型关联
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
