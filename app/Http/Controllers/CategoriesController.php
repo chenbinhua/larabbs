68192746a291b930425b02acfe1c183110f35bb1
chenbinghua HS255 <HS255@huishengziben.com>
2018-06-12 13:34:31 +0800

    Topic order

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -9,10 +9,12 @@ use App\Models\Category;
 class CategoriesController extends Controller
 {
     //
-    public function show(Category $category)
+    public function show(Category $category, Topic $topic, Request $request)
     {
         //读取分类ID关联话题，并按15分页
-        $topics = Topic::with('category','user')->where('category_id',$category->id)->paginate(15);
+        $topics = $topic->where('category_id',$category->id)
+                        ->withOrder($request->order)
+                        ->paginate(15);
         return view('topics.index',compact('topics', 'category'));
     }
 }
