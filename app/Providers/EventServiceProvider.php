7660abb04282db494c805556d19a086aecc7bfc2
chenbinghua HS255 <HS255@huishengziben.com>
2018-06-14 17:20:24 +0800

    User can create topic

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Topic;
+use App\Observers\TopicObserver;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -26,7 +28,7 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         parent::boot();
-
+        Topic::observe(new TopicObserver());
         //
     }
 }
